8f7c0703939e57b44d845424f107517138fa9696
Matheus <matheusmol@hotmail.com>
2021-04-30 22:37:18 -0300

    test add

---
+++ test/test.js
@@ -0,0 +1,106 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+const { assert } = require('chai');
+const supertest = require('supertest');
+const nock = require('nock');
+const Vehicle = require('../src/models/Vehicle');
+const index = require('../src/index');
+
+describe('Testes:', () => {
+  it('Teste-unitario', () => {
+    const body = {
+      placa: '6565',
+      chassi: 5656,
+      renavam: 65656,
+      modelo: 'ferrari',
+      marca: 'ferrari',
+      ano: 2021,
+    };
+    const result = new Vehicle(body);
+    assert.strictEqual(result.placa, Number(body.placa));
+    assert.strictEqual(result.chassi, Number(body.chassi));
+    assert.strictEqual(result.renavam, Number(body.renavam));
+    assert.strictEqual(result.modelo, body.modelo);
+    assert.strictEqual(result.marca, body.marca);
+    assert.strictEqual(result.ano, String(body.ano));
+  });
+
+  it('Post teste-p', (done) => {
+    nock('https://mt-node-stock-api.glitch.me')
+      .post('/vehicle')
+      .reply(200);
+    supertest(index.server)
+      .post('/vehicle')
+      .send({
+        placa: '111',
+        chassi: 111,
+        renavam: 111,
+        modelo: 'ferrari1',
+        marca: 'ferrari',
+        ano: 2021,
+      })
+      .expect(201)
+      .end(done);
+  });
+
+  it('Get /vehicles/ferrari', (done) => {
+    supertest(index.server)
+      .get('/vehicles/ferrari')
+      .expect(200)
+      .end(done);
+  });
+
+  it('Patch chassi 111 to 222', (done) => {
+    nock('https://mt-node-stock-api.glitch.me')
+      .post('/vehicle')
+      .reply(200);
+    supertest(index.server)
+      .patch('/vehicle/update/111')
+      .send({
+        placa: '111',
+        chassi: 222,
+        renavam: 111,
+        modelo: 'ferrari1',
+        marca: 'ferrari',
+        ano: 2021,
+      })
+      .expect(200)
+      .end(done);
+  });
+
+  it('Get chassi 111', (done) => {
+    supertest(index.server)
+      .get('/vehicles/chassi/ferrari')
+      .expect(401)
+      .end(done);
+  });
+
+  it('Delete placa 111', (done) => {
+    nock('https://mt-node-stock-api.glitch.me')
+      .post('/vehicle')
+      .reply(200);
+    supertest(index.server)
+      .delete('/vehicle/delete/111')
+      .send()
+      .expect(200)
+      .end(done);
+  });
+
+  // it('Teste da rota productNames', (done) => {
+  //   const resultadoEsperado = [
+  //     { name: 'cachorro', price: 110.59 },
+  //     { name: 'leao', price: 1000 },
+  //     { name: 'vaca', price: 75 },
+
+  //   ];
+  //   resultadoEsperado.sort((a, b) => a.price - b.price);
+  //   const newarr = [];
+  //   for (const prod in resultadoEsperado) {
+  //     newarr.push(resultadoEsperado[prod].name);
+  //   }
+  //   supertest(index.server)
+  //     .get('/productsNames')
+  //     .expect(200, newarr)
+  //     .end(done);
+  // });
+});
